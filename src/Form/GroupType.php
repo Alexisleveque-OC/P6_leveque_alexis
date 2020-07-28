6e4231a93e6ecec5de26fd478c88baba7d0bac72
LEVEQUE <alexis.leveque78@gmail.com>
2020-07-28 08:13:46 +0200

    little modif for design #24

--- src/Form/GroupType.php
+++ src/Form/GroupType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Group;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,11 @@ class GroupType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description', TextareaType::class)
-        ;
+            ->add('title', TextType::class, [
+                'label' => 'Titre'])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
