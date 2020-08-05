aa48e09420349585afc9dc14d9ffe98dc6bc1006
LEVEQUE <alexis.leveque78@gmail.com>
2020-08-05 08:27:05 +0200

    delete image  and video when delete trick

--- src/Service/Trick/DeleteTrick.php
+++ src/Service/Trick/DeleteTrick.php
@@ -4,6 +4,7 @@
 namespace App\Service\Trick;
 
 
+use App\Service\Upload\SaveImage;
 use Doctrine\ORM\EntityManagerInterface;
 
 class DeleteTrick
@@ -12,14 +13,27 @@ class DeleteTrick
      * @var EntityManagerInterface
      */
     private $entityManager;
+    /**
+     * @var SaveImage
+     */
+    private $saveImage;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager,SaveImage $saveImage)
     {
         $this->entityManager = $entityManager;
+        $this->saveImage = $saveImage;
     }
 
     public function delete($trick)
     {
+        foreach ($images = $trick->getImages() as $image){
+            $this->saveImage->deleteImageInServer($image);
+            $this->entityManager->remove($image);
+        }
+        foreach ($videos = $trick->getVideos() as $video){
+            $this->entityManager->remove($video);
+        }
+
         $this->entityManager->remove($trick);
         $this->entityManager->flush();
     }
